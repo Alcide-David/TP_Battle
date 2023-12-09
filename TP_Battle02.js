aec24633441578cb5f6033aaea15704822337829
Alcide-David <152320912+Alcide-David@users.noreply.github.com>
2023-12-09 17:08:41 -0500

    Add files via upload

---
+++ TP_Battle02.js
@@ -0,0 +1,22 @@
+function estAnagrame(mot1, mot2) {
+    if (mot1.length !== mot2.length) {
+      return false;
+    }
+  
+    const tab1 = mot1.split("");
+    const tab2 = mot2.split("");
+  
+    // Mélange karakte yo
+    tab1.sort();
+    tab2.sort();
+  
+    // Konparezon
+    for (let i = 0; i < tab1.length; i++) {
+      if (tab1[i] !== tab2[i]) {
+        return false;
+      }
+    }
+}
+let rezilta = estAnagrame("David", "divaD");
+console.info(rezilta);
+  
